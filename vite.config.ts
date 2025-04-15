bceba45bd956ce8d42a735b8d07da848172bac90
camicaillie <camicaillie@gmail.com>
2025-04-15 17:10:36 +0200

    url change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flashcards/', // Match the path from homepage in package.json
+  base: '/freekarticky/', // Match GitHub Pages repository name
 });
