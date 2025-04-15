0146d45419f46229182d7c0260b4369da0b56add
camicaillie <camicaillie@gmail.com>
2025-04-15 17:45:19 +0200

    new domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/freekarticky/', // Match GitHub Pages repository name
+  base: '/', // For custom domain, use root path
 });
